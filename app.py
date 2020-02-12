7ee3c88bf325b1f659bf0567fba011d4f888d156
Joao Santos <35111886+JoMiguelSantos@users.noreply.github.com>
2020-02-12 17:37:47 +0100

    cleanup syntax a bit

--- app.py
+++ app.py
@@ -11,15 +11,14 @@ def get_route():
     city = request.args.get("city")  # if key doesn't exist, returns None
     difficulty = request.args.get("difficulty")  # if key doesn't exist, returns None
     points_of_interest = subprocess.run(
+        #example test run
         [
-            # f"python3 /Users/joaosantos/GTH-Barcelona-AlTrips/middleware/point_selector.py --city={city} --difficulty={difficulty}"
-            "python3 /Users/joaosantos/GTH-Barcelona-AlTrips/middleware/point_selector.py --city=Berlin --difficulty=1"
+            "python3 point_selector.py --city=Berlin --difficulty=1"
         ],
         stdout=subprocess.PIPE,
         check=True,
         shell=True,
-        env={"G_API_KEY": "AIzaSyB8jdNn1SowwLsHvHd7ZWdlXd0vYlnNvCA"},
+        env={"G_API_KEY": "api__key"},
     ).stdout
-    # dir_path = os.path.dirname(os.path.realpath(__file__))
-    # points_of_interest = "gotcha"
+   
     return points_of_interest
